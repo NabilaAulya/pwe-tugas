bc9ef37d2c04b7bd2b63fddadba168614ee04892
NabilaAulya <nabilaaulya96@gmail.com>
2022-05-17 11:33:56 +0700

    menambahkan

---
+++ database/migrations/2022_05_17_041229_create_buku_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBukuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pengarang_id')->constrained('pengarang');
+            $table->decimal('harga',10,2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buku');
+    }
+}
